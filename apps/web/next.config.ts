502259e096c0f46f9aab50d8e7310efe365e0c7c
jagrat7 <jagratrao@gmail.com>
2026-05-23 11:28:17 -0700

    init

---
+++ apps/web/next.config.ts
@@ -0,0 +1,10 @@
+import "@my-better-t-app/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	typedRoutes: true,
+	reactCompiler: true,
+};
+
+export default nextConfig;
+
