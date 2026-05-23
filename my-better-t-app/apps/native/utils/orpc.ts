f3022defe604d9b690b071562965aa886cd7b70f
jagrat7 <jagratrao@gmail.com>
2026-05-23 11:43:16 -0700

    remove unused native app screens and web utilities, add expo ui building skill documentation

---
+++ my-better-t-app/apps/native/utils/orpc.ts
@@ -0,0 +1,22 @@
+import type { AppRouterClient } from "@my-better-t-app/api/routers/index";
+import { env } from "@my-better-t-app/env/native";
+import { createORPCClient } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      console.log(error);
+    },
+  }),
+});
+
+export const link = new RPCLink({
+  url: `${env.EXPO_PUBLIC_SERVER_URL}/api/rpc`,
+});
+
+export const client: AppRouterClient = createORPCClient(link);
+
+export const orpc = createTanstackQueryUtils(client);
